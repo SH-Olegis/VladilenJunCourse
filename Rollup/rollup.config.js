95dda5c3a59ee62caaca9b6b489350ee5dbd0d89
Oleg Shevchenko <oshevchenko@oggettoweb.com>
2021-09-04 14:19:50 +0300

    2 task

--- Rollup/rollup.config.js
+++ Rollup/rollup.config.js
@@ -1,7 +1,25 @@
+import styles from "rollup-plugin-styles";
+import image from 'rollup-plugin-img';
+import { babel } from '@rollup/plugin-babel';
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
+
 export default {
     input: './src/index.js',
     output: {
         file: './build/bundle.js',
         format: 'cjs'
-    }
+    },
+    plugins: [
+        styles(),
+        image({
+            limit: 10000000
+        }),
+        babel({ babelHelpers: 'bundled' }),
+        serve({
+            open: true,
+            contentBase: './'
+        }),
+        livereload()
+    ]
 };
