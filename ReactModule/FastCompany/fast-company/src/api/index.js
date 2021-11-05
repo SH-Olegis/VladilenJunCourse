6c03ecce73af75c42950c5569bebf7c1f36b75a0
Oleg Shevchenko <oshevchenko@oggettoweb.com>
2021-11-05 11:00:11 +0300

    Restart module react

--- ReactModule/FastCompany/fast-company/src/api/index.js
+++
@@ -1,7 +0,0 @@
-import users from "./fake.api/user.api"
-import professions from "./fake.api/proffesions.api"
-const API = {
-    users,
-    professions
-}
-export default API
--- ReactModule/FirstProject/my-app/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-
-const title = <h1>Hello world меня зовут: Олег</h1>
-
-ReactDom.render(title, document.getElementById('root'))
